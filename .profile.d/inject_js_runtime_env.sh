a3743a5f2644f277ec939cf65840e8eb165934fb
Jack Griffin <jackpg14@gmail.com>
2023-04-05 13:17:29 -0400

    Inject runtime env in all js bundles so we dont have to guess

--- .profile.d/inject_js_runtime_env.sh
+++ .profile.d/inject_js_runtime_env.sh
@@ -7,14 +7,18 @@ JS_RUNTIME_ENV_PREFIX="${JS_RUNTIME_ENV_PREFIX:-JS_RUNTIME_}"
 
 # Each bundle is generated with a unique hash name to bust browser cache.
 # Use shell `*` globbing to fuzzy match.
-JS_RUNTIME_TARGET_BUNDLE="${JS_RUNTIME_TARGET_BUNDLE:-/app/build/index.*.js}"
+js_bundles="${JS_RUNTIME_TARGET_BUNDLE:-/app/build/static/js/*.js}"
+# Get exact filenames.
+js_bundle_filenames=`ls $js_bundles`
 
-if [ -f $JS_RUNTIME_TARGET_BUNDLE ]
+if [ ! "$?" = 0 ]
 then
+  echo "Error injecting runtime env: bundle not found '$js_bundles'. See: https://github.com/mars/create-react-app-buildpack/blob/master/README.md#user-content-custom-bundle-location"
+fi
 
-  # Get exact filename.
-  js_bundle_filename=`ls $JS_RUNTIME_TARGET_BUNDLE`
-  
+for js_bundle_filename in $js_bundle_filenames
+do
+
   echo "Injecting runtime env into $js_bundle_filename (from .profile.d/inject_js_runtime_env.sh)"
 
   # Render runtime env vars into bundle.
